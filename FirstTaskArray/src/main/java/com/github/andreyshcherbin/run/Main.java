0dd1a429dbc5f8649162199519db1868078578df
andreyscherbin <andrey123scherbin@gmail.com>
2021-01-26 15:29:53 +0300

    update

---
+++ FirstTaskArray/src/main/java/com/github/andreyshcherbin/run/Main.java
@@ -0,0 +1,13 @@
+package com.github.andreyshcherbin.run;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Main {
+	
+	static Logger logger = LogManager.getLogger();
+
+	public static void main(String[] args) {
+		logger.info("start application");
+	}
+}
